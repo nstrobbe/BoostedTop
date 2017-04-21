0398e6916bae4c12f14f6d11b671f8f483825c4d
Nadja Strobbe <nadja.strobbe@cern.ch>
2017-04-21 10:46:21 -0500

    update to have more angles and more files

--- computeAngles.cpp
+++ computeAngles.cpp
@@ -14,9 +14,9 @@ using namespace std;
 
 void computeAngles(TLorentzVector thep4H, TLorentzVector thep4Z1, TLorentzVector thep4M11, TLorentzVector thep4M12, TLorentzVector thep4Z2, TLorentzVector thep4M21, TLorentzVector thep4M22, double& costheta1, double& costheta2, double& Phi, double& costhetastar, double& Phi1);
 
-void computeAnglesTTbar(TLorentzVector p4b1, TLorentzVector p4W1u, TLorentzVector p4W1d, TLorentzVector p4b2, TLorentzVector p4W2u, TLorentzVector p4W2d,
+void computeAnglesTTbar(const TLorentzVector p4b1, const TLorentzVector p4W1u, const TLorentzVector p4W1d, const TLorentzVector p4b2, const TLorentzVector p4W2u, const TLorentzVector p4W2d,
 			double& costhetastar, double& costheta1, double& costheta2, double& costhetaW1, double& costhetaW2, 
-			double& Phi, double& Phi1, double& PhiW1, double& PhiW2);
+			double& Phi, double& Phi1, double& Phi2, double& PhiW1, double& PhiW2);
 
 double deltaPhi (double phi1, double phi2)
 {
@@ -35,15 +35,13 @@ int main (int argc, char **argv) {
     TFile file(argv[2],"RECREATE");
     TTree* tree = new TTree("tree","tree");
     
-    //float mWW, mWLep, mWHad; 
+    float mttbar; 
     float costheta1, costheta2, costhetastar, costhetaW1, costhetaW2;
-    float phi, phi1, phiW1, phiW2;
+    float phi, phi1, phi2, phiW1, phiW2;
     //float dEtajj, dPhijj, mjj;
-    //int isSignal,isMuMinus;
+    float dPhi12, dPhiW1W2, dPhi1W1, dPhi2W2;
     
-    //tree->Branch("mWW",&mWW,"mWW/F");
-    //tree->Branch("mWLep",&mWLep,"mWLep/F");
-    //tree->Branch("mWHad",&mWHad,"mWHad/F");
+    tree->Branch("mttbar",&mttbar,"mttbar/F");
     tree->Branch("costheta1",&costheta1,"costheta1/F");
     tree->Branch("costheta2",&costheta2,"costheta2/F");
     tree->Branch("costhetaW1",&costhetaW1,"costhetaW1/F");
@@ -51,14 +49,17 @@ int main (int argc, char **argv) {
     tree->Branch("costhetastar",&costhetastar,"costhetastar/F");
     tree->Branch("phi",&phi,"phi/F");
     tree->Branch("phi1",&phi1,"phi1/F");
+    tree->Branch("phi2",&phi2,"phi2/F");
     tree->Branch("phiW1",&phiW1,"phiW1/F");
     tree->Branch("phiW2",&phiW2,"phiW2/F");
     //tree->Branch("dEtajj",&dEtajj,"dEtajj/F");
     //tree->Branch("dPhijj",&dPhijj,"dPhijj/F");
     //tree->Branch("mjj",&mjj,"mjj/F");
+    tree->Branch("dPhi12",&dPhi12,"dPhi12/F");
+    tree->Branch("dPhiW1W2",&dPhiW1W2,"dPhiW1W2/F");
+    tree->Branch("dPhi1W1",&dPhi1W1,"dPhi1W1/F");
+    tree->Branch("dPhi2W2",&dPhi2W2,"dPhi2W2/F");
     
-    //tree->Branch("isSignal",&isSignal,"isSignal/I");
-    //tree->Branch("isMuMinus",&isMuMinus,"isMuMinus/I");
     
     //PG loop over bkg
     //PG -------------
@@ -98,7 +99,6 @@ int main (int argc, char **argv) {
 
         //PG loop over particles in the event
         for (int iPart = 0 ; iPart < bkgReader.hepeup.IDUP.size (); ++iPart){
-            
             int mother1 = bkgReader.hepeup.MOTHUP.at(iPart).first - 1;
 	    int mother1ID = -1;
 	    if (mother1 != -1)
@@ -126,12 +126,12 @@ int main (int argc, char **argv) {
 		// find b quarks from top quark
 		if( myID == 5 && mother1ID == 6)
 		    i_b_1 = iPart;
-		else if( myID == 5 && mother1ID == -6) // in case there is a three-body decay
-		    i_Wd_2 = iPart;
+		//else if( myID == 5 && mother1ID == -6) // in case there is a three-body decay
+		//    i_Wd_2 = iPart;
 		else if( myID == -5 && mother1ID == -6)
 		    i_b_2 = iPart;
-		else if( myID == -5 && mother1ID == 6)
-		    i_Wd_1 = iPart; 
+		//else if( myID == -5 && mother1ID == 6)
+		//    i_Wd_1 = iPart; 
 		// other quarks and leptons from W decay (could be three-body decay)
 		else if( mother1ID == 24 || mother1ID == 6)
 		{
@@ -141,7 +141,7 @@ int main (int argc, char **argv) {
 			i_Wd_1 = iPart;
 		}
 		// And W- decay products
-		if( mother1ID == -24 || mother1ID == -6)
+		else if( mother1ID == -24 || mother1ID == -6)
 		{
 		    if( myID == -2 || myID == -4 || myID == -12 || myID == -14 || myID == -16)
 			i_Wu_2 = iPart;
@@ -184,6 +184,13 @@ int main (int argc, char **argv) {
 
         } //PG loop over particles in the event
 
+	//std::cout << "i_b_1: " << i_b_1 << std::endl;
+	//std::cout << "i_Wu_1: " << i_Wu_1 << std::endl;
+	//std::cout << "i_Wd_1: " << i_Wd_1 << std::endl;
+	//std::cout << "i_b_2: " << i_b_2 << std::endl;
+	//std::cout << "i_Wu_2: " << i_Wu_2 << std::endl;
+	//std::cout << "i_Wd_2: " << i_Wd_2 << std::endl;
+
         TLorentzVector fs_b_1(
 	    bkgReader.hepeup.PUP.at(i_b_1).at(0), //PG px
 	    bkgReader.hepeup.PUP.at(i_b_1).at(1), //PG py
@@ -225,9 +232,9 @@ int main (int argc, char **argv) {
         //TLorentzVector p4_W1 = fs_Wu_1 + fs_Wd_1;
         //TLorentzVector p4_W2 = fs_Wu_2 + fs_Wd_2;        
         
-        double a_costheta1, a_costheta2, a_costhetastar, a_costhetaW1, a_costhetaW2, a_Phi, a_Phi1, a_PhiW1, a_PhiW2;
+        double a_costheta1, a_costheta2, a_costhetastar, a_costhetaW1, a_costhetaW2, a_Phi, a_Phi1, a_Phi2, a_PhiW1, a_PhiW2;
         computeAnglesTTbar( fs_b_1, fs_Wu_1, fs_Wd_1, fs_b_2, fs_Wu_2, fs_Wd_2,
-			    a_costhetastar, a_costheta1, a_costheta2, a_costhetaW1, a_costhetaW2, a_Phi, a_Phi1, a_PhiW1, a_PhiW2);
+			    a_costhetastar, a_costheta1, a_costheta2, a_costhetaW1, a_costhetaW2, a_Phi, a_Phi1, a_Phi2, a_PhiW1, a_PhiW2);
 
 	costheta1 = (float) a_costheta1;                
         costheta2 = (float) a_costheta2;
@@ -236,9 +243,33 @@ int main (int argc, char **argv) {
         phi = (float) a_Phi;
         costhetastar = (float) a_costhetastar;
         phi1 = (float) a_Phi1;
+        phi2 = (float) a_Phi2;
         phiW1 = (float) a_PhiW1;
         phiW2 = (float) a_PhiW2;
 
+	dPhi12 = deltaPhi(phi1, phi2);
+	dPhiW1W2 = deltaPhi(phiW1, phiW2);
+	dPhi1W1 = deltaPhi(phi1, phiW1);
+	dPhi2W2 = deltaPhi(phi2, phiW2);
+
+	if(tops.size() == 2){
+	    TLorentzVector t_1(
+		bkgReader.hepeup.PUP.at(tops[0]).at(0), //PG px
+		bkgReader.hepeup.PUP.at(tops[0]).at(1), //PG py
+		bkgReader.hepeup.PUP.at(tops[0]).at(2), //PG pz
+		bkgReader.hepeup.PUP.at(tops[0]).at(3) //PG E
+		) ;
+	    TLorentzVector t_2(
+		bkgReader.hepeup.PUP.at(tops[1]).at(0), //PG px
+		bkgReader.hepeup.PUP.at(tops[1]).at(1), //PG py
+		bkgReader.hepeup.PUP.at(tops[1]).at(2), //PG pz
+		bkgReader.hepeup.PUP.at(tops[1]).at(3) //PG E
+		) ;
+	    mttbar = (t_1 + t_2).M();
+	} else { 
+	    mttbar = -1;
+	}
+	    
 /*
        
         //mWW = (float) p4_WW.M();
@@ -270,12 +301,12 @@ return 0 ;
 //// P A P E R   4 - V E C T O R   D E F I N I T I O N   O F   P H I   A N D   P H I 1
 //////////////////////////////////
 void computeAnglesTTbar(
-  TLorentzVector p4b1,
-  TLorentzVector p4W1u,
-  TLorentzVector p4W1d,
-  TLorentzVector p4b2, 
-  TLorentzVector p4W2u,
-  TLorentzVector p4W2d,
+  const TLorentzVector p4b1,
+  const TLorentzVector p4W1u,
+  const TLorentzVector p4W1d,
+  const TLorentzVector p4b2, 
+  const TLorentzVector p4W2u,
+  const TLorentzVector p4W2d,
   double& costhetastar,
   double& costheta1,
   double& costheta2,
@@ -283,6 +314,7 @@ void computeAnglesTTbar(
   double& costhetaW2,
   double& Phi,
   double& Phi1,
+  double& Phi2,
   double& PhiW1,
   double& PhiW2
   ){
@@ -370,55 +402,55 @@ void computeAnglesTTbar(
   TVector3 boostV2_W(0, 0, 0);
   //// --------------------------- costhetaW1
   //if (!(fabs(t1_bId)==5 || fabs(t1_uId)==2 || fabs(t1_dId)==1 || fabs(t1_uId)==4 || fabs(t1_dId)==3 )){ // can probably remove this for now?
-    boostV1_W = -(p4W1.BoostVector());
-    if (boostV1_W.Mag()>=1.) {
+  boostV1_W = -(p4W1.BoostVector());
+  if (boostV1_W.Mag()>=1.) {
       cout << "Warning: Mela::computeAngles: W1 boost with beta=1, scaling down" << endl;
       boostV1_W*=0.9999/boostV1_W.Mag();
-    }
-    TLorentzVector p4b1_BV1_W(p4b1);
-    TLorentzVector p4W1u_BV1_W(p4W1u);
-    TLorentzVector p4W1d_BV1_W(p4W1d);
-    TLorentzVector p4b2_BV1_W(p4b2);
-    TLorentzVector p4W2u_BV1_W(p4W2u);
-    TLorentzVector p4W2d_BV1_W(p4W2d);
-    p4b1_BV1_W.Boost(boostV1_W);
-    p4W1u_BV1_W.Boost(boostV1_W);
-    p4W1d_BV1_W.Boost(boostV1_W);
-    p4b2_BV1_W.Boost(boostV1_W);
-    p4W2u_BV1_W.Boost(boostV1_W);
-    p4W2d_BV1_W.Boost(boostV1_W);
-
-    TLorentzVector p4V2_BV1_W = p4b1_BV1_W;
-    //// costheta1
-    costhetaW1 = -p4V2_BV1_W.Vect().Unit().Dot(p4W1d_BV1_W.Vect().Unit());
-    //}
-    //else costhetaW1 = 0;
-
+  }
+  TLorentzVector p4b1_BV1_W(p4b1);
+  TLorentzVector p4W1u_BV1_W(p4W1u);
+  TLorentzVector p4W1d_BV1_W(p4W1d);
+  TLorentzVector p4b2_BV1_W(p4b2);
+  TLorentzVector p4W2u_BV1_W(p4W2u);
+  TLorentzVector p4W2d_BV1_W(p4W2d);
+  p4b1_BV1_W.Boost(boostV1_W);
+  p4W1u_BV1_W.Boost(boostV1_W);
+  p4W1d_BV1_W.Boost(boostV1_W);
+  p4b2_BV1_W.Boost(boostV1_W);
+  p4W2u_BV1_W.Boost(boostV1_W);
+  p4W2d_BV1_W.Boost(boostV1_W);
+  
+  TLorentzVector p4V2_BV1_W = p4b1_BV1_W;
+  //// costheta1
+  costhetaW1 = (-p4V2_BV1_W).Vect().Unit().Dot(p4W1d_BV1_W.Vect().Unit());
+  //}
+  //else costhetaW1 = 0;
+  
   //// --------------------------- costhetaW2
   //if (!(fabs(t1_bId)==5 || fabs(t1_uId)==2 || fabs(t1_dId)==1 || fabs(t1_uId)==4 || fabs(t1_dId)==3 )){
-    boostV2_W = -(p4W2.BoostVector());
-    if (boostV2_W.Mag()>=1.) {
+  boostV2_W = -(p4W2.BoostVector());
+  if (boostV2_W.Mag()>=1.) {
       cout << "Warning: Mela::computeAngles: W2 boost with beta=1, scaling down" << endl;
       boostV2_W*=0.9999/boostV2_W.Mag();
-    }
-    TLorentzVector p4b1_BV2_W(p4b1);
-    TLorentzVector p4W1u_BV2_W(p4W1u);
-    TLorentzVector p4W1d_BV2_W(p4W1d);
-    TLorentzVector p4b2_BV2_W(p4b2);
-    TLorentzVector p4W2u_BV2_W(p4W2u);
-    TLorentzVector p4W2d_BV2_W(p4W2d);
-    p4b1_BV2_W.Boost(boostV2_W);
-    p4W1u_BV2_W.Boost(boostV2_W);
-    p4W1d_BV2_W.Boost(boostV2_W);
-    p4b2_BV2_W.Boost(boostV2_W);
-    p4W2u_BV2_W.Boost(boostV2_W);
-    p4W2d_BV2_W.Boost(boostV2_W);
-
-    TLorentzVector p4V1_BV2_W = p4b2_BV2_W;
-    //// costheta2
-    costhetaW2 = -p4V1_BV2_W.Vect().Unit().Dot(p4W2d_BV2_W.Vect().Unit());
-    //}
-    //else costhetaW2 = 0;
+  }
+  TLorentzVector p4b1_BV2_W(p4b1);
+  TLorentzVector p4W1u_BV2_W(p4W1u);
+  TLorentzVector p4W1d_BV2_W(p4W1d);
+  TLorentzVector p4b2_BV2_W(p4b2);
+  TLorentzVector p4W2u_BV2_W(p4W2u);
+  TLorentzVector p4W2d_BV2_W(p4W2d);
+  p4b1_BV2_W.Boost(boostV2_W);
+  p4W1u_BV2_W.Boost(boostV2_W);
+  p4W1d_BV2_W.Boost(boostV2_W);
+  p4b2_BV2_W.Boost(boostV2_W);
+  p4W2u_BV2_W.Boost(boostV2_W);
+  p4W2d_BV2_W.Boost(boostV2_W);
+  
+  TLorentzVector p4V1_BV2_W = p4b2_BV2_W;
+  //// costheta2
+  costhetaW2 = -p4V1_BV2_W.Vect().Unit().Dot(p4W2d_BV2_W.Vect().Unit());
+  //}
+  //else costhetaW2 = 0;
 
   //// --------------------------- Phi and Phi1 (old phistar1 - azimuthal production angle)
   TLorentzVector p4b1_BX(p4b1);
@@ -440,18 +472,22 @@ void computeAnglesTTbar(
 
   TVector3 beamAxis(0, 0, 1);
   TVector3 p3V1_BX = p4V1_BX.Vect().Unit();
-  TVector3 normal1_BX = (p4b1_BX.Vect().Cross(p4W1_BX.Vect())).Unit();
+  TVector3 normal1_BX = (p4b1_BX.Vect().Cross(p4W1_BX.Vect())).Unit(); // perpendicular to the plane formed by the b+W from the top1 decay
   TVector3 normal2_BX = (p4b2_BX.Vect().Cross(p4W2_BX.Vect())).Unit();
-  TVector3 normalSC_BX = (beamAxis.Cross(p3V1_BX)).Unit();
+  TVector3 normalW1_BX = (p4W1u_BX.Vect().Cross(p4W1d_BX.Vect())).Unit(); // perpendicular to the plane formed by the b+W from the top1 decay
+  TVector3 normalW2_BX = (p4W2u_BX.Vect().Cross(p4W2d_BX.Vect())).Unit();
+  TVector3 normalSC_BX = (beamAxis.Cross(p3V1_BX)).Unit(); //perpendicular to the plane formed by the beam and the top1
 
 
-  //// Phi
-  float tmpSgnPhi = p3V1_BX.Dot(normal1_BX.Cross(normal2_BX));
+  //// Phi : angle between planes formed by top decays
+  float tmpSgnPhi = p3V1_BX.Dot(normal1_BX.Cross(normal2_BX)); //(normal1 Cross normal2) should be the line shared by the two planes 
   float sgnPhi = 0;
   if (fabs(tmpSgnPhi)>0.) sgnPhi = tmpSgnPhi/fabs(tmpSgnPhi);
-  float dot_BX12 = normal1_BX.Dot(normal2_BX);
-  if (fabs(dot_BX12)>=1.) dot_BX12 *= 1./fabs(dot_BX12);
-  Phi = sgnPhi * acos(-1.*dot_BX12);
+  // so the sign is defined to be positive if the projection of the vector that is common for the two planes onto the top1 vector is positive
+  float dot_BX12 = normal1_BX.Dot(normal2_BX); // a dot b = |a|*|b|*cos(phi), and a and b are unit vectors here
+  if (fabs(dot_BX12)>=1.) dot_BX12 *= 1./fabs(dot_BX12); // should never occur
+  Phi = sgnPhi * acos(-1.*dot_BX12); // why a minus sign in the acos?
+  //std::cout << "dot: " << dot_BX12 << ", sign: " << sgnPhi << ", Phi: " << Phi << endl;
 
 
   //// Phi1
@@ -461,6 +497,32 @@ void computeAnglesTTbar(
   double dot_BX1SC = normal1_BX.Dot(normalSC_BX);
   if (fabs(dot_BX1SC)>=1.) dot_BX1SC *= 1./fabs(dot_BX1SC);
   Phi1 = sgnPhi1 * acos(dot_BX1SC);
+
+  //// Phi2
+  double tmpSgnPhi2 = p3V1_BX.Dot(normal2_BX.Cross(normalSC_BX));
+  double sgnPhi2 = 0;
+  if (fabs(tmpSgnPhi2)>0.) sgnPhi2 = tmpSgnPhi2/fabs(tmpSgnPhi2);
+  double dot_BX2SC = normal2_BX.Dot(normalSC_BX);
+  if (fabs(dot_BX2SC)>=1.) dot_BX2SC *= 1./fabs(dot_BX2SC);
+  Phi2 = sgnPhi2 * acos(dot_BX2SC);
+
+  //std::cout << "phi: " << Phi << ", phi1: " << Phi1 << ", phi2: " << Phi2 << std::endl;
+
+  //// PhiW1
+  double tmpSgnPhiW1 = p3V1_BX.Dot(normalW1_BX.Cross(normalSC_BX));
+  double sgnPhiW1 = 0;
+  if (fabs(tmpSgnPhiW1)>0.) sgnPhiW1 = tmpSgnPhiW1/fabs(tmpSgnPhiW1);
+  double dot_BXW1SC = normalW1_BX.Dot(normalSC_BX);
+  if (fabs(dot_BXW1SC)>=1.) dot_BXW1SC *= 1./fabs(dot_BXW1SC);
+  PhiW1 = sgnPhiW1 * acos(dot_BXW1SC);
+
+  //// PhiW2
+  double tmpSgnPhiW2 = p3V1_BX.Dot(normalW2_BX.Cross(normalSC_BX));
+  double sgnPhiW2 = 0;
+  if (fabs(tmpSgnPhiW2)>0.) sgnPhiW2 = tmpSgnPhiW2/fabs(tmpSgnPhiW2);
+  double dot_BXW2SC = normalW2_BX.Dot(normalSC_BX);
+  if (fabs(dot_BXW2SC)>=1.) dot_BXW2SC *= 1./fabs(dot_BXW2SC);
+  PhiW2 = sgnPhiW2 * acos(dot_BXW2SC);
 
   if (isnan(costhetastar) || isnan(costheta1) || isnan(costheta2) || isnan(Phi) || isnan(Phi1)){
     cout << "WARNING: NaN in computeAngles: "
--- plotting/plot.py
+++ plotting/plot.py
@@ -13,65 +13,95 @@ ROOT.setTDRStyle();
 ROOT.gStyle.SetPadLeftMargin(0.16);
 
 
+def getMax(histos):
+    temp_max = 0
+    for h in histos:
+        hmax = h.GetMaximum()
+        if hmax>temp_max:
+            temp_max = hmax
+    return temp_max
+
 ############################################################
 
 if __name__ == '__main__':
 
-    file_gg_s = ROOT.TFile("../out_gg_s.root");
-    file_pp_zp = ROOT.TFile("../out_pp_zp.root");
-    tree_gg_s = file_gg_s.Get("tree");
-    tree_pp_zp = file_pp_zp.Get("tree");
-    
-    vars = ["costheta1","costheta2","costhetastar","phi","phi1"]
-    vars_x = ["costheta1","costheta2","costhetastar","phi","phi1"]
-    vars_nbin = [20]*5;
-    vars_lo = [-1,   0,   -1, -ROOT.TMath.Pi(), -ROOT.TMath.Pi()]
-    vars_hi = [ 1,   1,    1,  ROOT.TMath.Pi(),  ROOT.TMath.Pi()]    
-    
-    h_gg_s = [];
-    h_pp_zp = [];
-    
-    for i in range(len(vars)):
-        h_gg_s.append( ROOT.TH1F("h_gg_s_"+vars[i],";"+vars_x[i]+";n.d.",vars_nbin[i],vars_lo[i],vars_hi[i]) );
-        h_pp_zp.append( ROOT.TH1F("h_pp_zp_"+vars[i],";"+vars_x[i]+";n.d.",vars_nbin[i],vars_lo[i],vars_hi[i]) );   
+    filenames = [#"../out_gg_s.root",
+                 #"../out_gg_a.root",
+                 #"../out_pp_zp.root",
+                 "../out_pp_zpbl.root",
+                 "../out_pp_zpchi.root",
+                 "../out_pp_zpeta.root",
+                 "../out_pp_zplr.root",
+                 "../out_pp_zppsi.root",
+                 "../out_pp_zpssm.root"
+                 ]
+    labels = [f.split("/")[-1].strip(".root").lstrip("out_") for f in filenames]
+    files = [ROOT.TFile(fname) for fname in filenames]
+    colors = [#ROOT.kBlack,
+              #ROOT.kGray+1,
+              #ROOT.kRed,
+              ROOT.kMagenta,
+              ROOT.kBlue,
+              ROOT.kGreen+2,
+              ROOT.kMagenta+2,
+              ROOT.kOrange,
+              ROOT.kCyan+1]
 
+    trees = [f.Get("tree") for f in files]
     
-    for i in range( tree_gg_s.GetEntries() ):
-        tree_gg_s.GetEntry(i);
-        if i%2000 == 0: print "i = ",i
-        
-        for j in range(len(vars)):
-            h_gg_s[j].Fill( getattr( tree_gg_s, vars[j] ) );
+    vars = ["costheta1","costheta2","costhetastar","phi","phi1","costhetaW1","costhetaW2","phi2","phiW1","phiW2",
+            "dPhi12","dPhiW1W2","dPhi1W1","dPhi2W2","mttbar"]
+    vars_x = ["costheta1","costheta2","costhetastar","phi","phi1","costhetaW1","costhetaW2","phi2","phiW1","phiW2",
+              "#Delta(Phi1,Phi2)","#Delta(PhiW1,PhiW2)","#Delta(Phi1,PhiW1)","#Delta(Phi2,PhiW2)","m(ttbar)"]
+    vars_nbin = [20]*len(vars);
+    vars_lo = [-1,  -1,   -1, -ROOT.TMath.Pi(), -ROOT.TMath.Pi(), -1,   -1, -ROOT.TMath.Pi(), -ROOT.TMath.Pi(), -ROOT.TMath.Pi(),
+                0, 0, 0, 0, 0]
+    vars_hi = [ 1,   1,    1,  ROOT.TMath.Pi(),  ROOT.TMath.Pi(),  1,    1,  ROOT.TMath.Pi(),  ROOT.TMath.Pi(),  ROOT.TMath.Pi(),
+                ROOT.TMath.Pi(),  ROOT.TMath.Pi(),  ROOT.TMath.Pi(), ROOT.TMath.Pi(), 3000]    
+    
+    histos = []
+    for l in labels:
+        histo_l = []
+        for i in range(len(vars)):
+            histo_l.append( ROOT.TH1F("h_"+l+"_"+vars[i],";"+vars_x[i]+";n.d.",vars_nbin[i],vars_lo[i],vars_hi[i]) )
+        histos.append(histo_l)
+    
+    for i_t, t in enumerate(trees):
+        for i in range( t.GetEntries()):
+            t.GetEntry(i)
+            if i%2000 == 0: print "i = ",i
+            
+            for j in range(len(vars)):
+                histos[i_t][j].Fill( getattr( t, vars[j] ) )
 
-    for i in range( tree_pp_zp.GetEntries() ):
-        tree_pp_zp.GetEntry(i);
-        if i%2000 == 0: print "i = ",i
-
-        for j in range(len(vars)):
-            h_pp_zp[j].Fill( getattr( tree_pp_zp, vars[j] ) );
                 
     ##### plot
     # scale and color
-    for j in range(len(vars)):
-        h_gg_s[j].Scale( 1./h_gg_s[j].Integral() );
-        h_pp_zp[j].Scale( 1./h_pp_zp[j].Integral() );
-        h_pp_zp[j].SetLineColor( 2 )
-        h_pp_zp[j].SetLineWidth( 2 )
-        h_gg_s[j].SetLineWidth( 2 )
+    for i_h, h in enumerate(histos):
+        for j in range(len(vars)):
+            h[j].Scale( 1./h[j].Integral() );
+            h[j].SetLineColor( colors[i_h] )
+            h[j].SetLineWidth( 3 )
         
     # on canvas
     for j in range(len(vars)):
         
         tmpcan = ROOT.TCanvas("can"+str(j),"can"+str(j),800,800);
-        h_gg_s[j].SetMaximum( 1.2*max( h_gg_s[j].GetMaximum(), h_pp_zp[j].GetMaximum() ) );
-        h_gg_s[j].SetMinimum( 0 );        
-        h_gg_s[j].Draw();
-        h_pp_zp[j].Draw("same");
+        tmpcan.SetRightMargin(0.05)
 
-        leg = ROOT.TLegend(0.25, 0.2, 0.5, 0.45)
+        leg = ROOT.TLegend(0.25, 0.75, 0.92, 0.92)
         leg.SetBorderSize(0)
-        leg.AddEntry(h_gg_s[j], "gg_s", "l")
-        leg.AddEntry(h_pp_zp[j], "pp_zp", "l")
+        leg.SetNColumns(2)
+        max = getMax( [h[j] for h in histos]  )
+        for i_h, h in enumerate(histos):
+            h[j].SetMaximum(1.5*max)
+            h[j].SetMinimum(0)
+            if i_h == 0:
+                h[j].Draw()
+            else:
+                h[j].Draw("same")
+            leg.AddEntry(h[j], labels[i_h], "l")
+
         leg.Draw()
 
         tmpcan.SaveAs("plots/"+vars[j]+".eps");
